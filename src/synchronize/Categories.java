1c5babf49b255ee994108d9f7419c263ce94e414
Lars Rasmussen <lars@1337.dk>
2013-05-07 22:24:29 +0200

    Removed unused imports and changed JAR file name

--- src/synchronize/Categories.java
+++ src/synchronize/Categories.java
@@ -1,11 +1,6 @@
 package synchronize;
 
 import java.net.URL;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.apache.pivot.beans.BXML;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
