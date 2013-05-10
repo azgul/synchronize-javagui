826867d145dd46a6311f0d077355fcfa33674664
Randi Hillerøe <silwing@gmail.com>
2013-05-11 01:32:28 +0200

    Added a resulttask to get more responsive search.
    This is not working as intended. For some reason the first seach always
    takes around 500 s, while subsequent searches takes less than 100 ms,
    both for same and different search terms.

--- src/synchronize/gui/SearchResults.java
+++ src/synchronize/gui/SearchResults.java
@@ -4,17 +4,19 @@
  */
 package synchronize.gui;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+
 import org.apache.pivot.beans.BXML;
-import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
-import org.apache.pivot.serialization.SerializationException;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.util.concurrent.Task;
+import org.apache.pivot.util.concurrent.TaskListener;
 import org.apache.pivot.wtk.FillPane;
 import org.apache.pivot.wtk.ScrollPane;
+import org.apache.pivot.wtk.TaskAdapter;
+
 import pdfsearch.SearchResult;
 
 /**
@@ -32,25 +34,21 @@ public class SearchResults extends ScrollPane implements Bindable {
 		results.removeAll();
 		
 		
-		for( SearchResult item : items ){
-			try{
-				BXMLSerializer bxmlSerializer = new BXMLSerializer();
-				SearchResultItem result = (SearchResultItem)bxmlSerializer.readObject(SearchResultItem.class, "/synchronize/bxml/searchresultitem.bxml");
-				//result.setAbstract(item.getAbstract());
-				result.setAbstract("Bacon ipsum dolor sit amet meatball drumstick spare ribs capicola, salami corned beef t-bone andouille cow short ribs ham. Sirloin turkey t-bone doner hamburger frankfurter ham hock short ribs pig cow venison jerky. Capicola tenderloin ground round, venison shoulder bresaola pork hamburger chicken pastrami ham hock chuck tail short loin brisket.");
-				
-				result.setModifiedDate(item.getModifiedDate());
-				result.setTitle("(" + item.getCategory() + ") " + item.getTitle());
-				result.setLanguage(SearchResultItem.LANG.getLanguage(item.getLanguage()));
-				result.setPdfPath(item.getPdf().getAbsolutePath());
-				
-				results.add(result);
-			}catch(IOException e){
-				e.printStackTrace();
-			}catch(SerializationException e){
-				e.printStackTrace();
-			}
-		}
+		ResultTask resultTask = new ResultTask(items, results);
+        TaskListener<Void> taskListener = new TaskListener<Void>() {
+            @Override
+            public void taskExecuted(Task<Void> task) {
+                System.out.println("Added search results.");
+            }
+
+            @Override
+            public void executeFailed(Task<Void> task) {
+
+                System.err.println(task.getFault());
+            }
+        };
+
+        resultTask.execute(new TaskAdapter<Void>(taskListener));
 		
 	}
 
