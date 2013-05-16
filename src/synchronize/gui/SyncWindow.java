e27af0c196fa170324b4f57702e4ae282d654579
Lars Rasmussen <lars@1337.dk>
2013-05-16 11:25:53 +0200

    Changing category image upon click on category in treeview
    When clicking in the treeview, the clicked category (if it was checked
    and not unchecked) will be notified and the category image will be
    updated.

--- src/synchronize/gui/SyncWindow.java
+++ src/synchronize/gui/SyncWindow.java
@@ -11,6 +11,7 @@ import org.apache.pivot.wtk.Border;
 import org.apache.pivot.wtk.Component;
 import org.apache.pivot.wtk.FileBrowserSheet;
 import org.apache.pivot.wtk.Frame;
+import org.apache.pivot.wtk.ImageView;
 import org.apache.pivot.wtk.MenuBar;
 import org.apache.pivot.wtk.MenuHandler;
 import org.apache.pivot.wtk.TabPane;
@@ -18,12 +19,16 @@ import org.apache.pivot.wtk.TextInput;
 import org.apache.pivot.wtk.TextInputContentListener;
 import org.apache.pivot.wtk.TextInputSelectionListener;
 import org.apache.pivot.wtk.TreeView;
+import synchronize.core.UIObservers;
+import synchronize.listeners.CategoryListener;
+import synchronize.model.Category;
 
-public class SyncWindow extends Frame implements Bindable {
+public class SyncWindow extends Frame implements Bindable {	
     @BXML private FileBrowserSheet fileBrowserSheet;
     @BXML private TabPane tabPane;
     @BXML private TreeView categories;
     @BXML public Border progress;
+	@BXML private ImageView categoryImage;
 
     private MenuHandler menuHandler = new MenuHandler.Adapter() {
         TextInputContentListener textInputTextListener = new TextInputContentListener.Adapter() {
@@ -98,6 +103,16 @@ public class SyncWindow extends Frame implements Bindable {
             }
         });
     }
+	
+	private void setCategoryImage(String image){
+		try{
+			System.out.println("Setting image to: " + image);
+			URL path = new URL(image);
+			categoryImage.setImage(path);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
     @Override
     public void initialize(Map<String, Object> namespace, URL location, Resources resources) {
@@ -106,5 +121,20 @@ public class SyncWindow extends Frame implements Bindable {
 		backDecorator.setShadowColor("#000000");
 		backDecorator.setShadowOpacity(0.8f);
 		backBorder.getDecorators().add(backDecorator);*/
+		
+		UIObservers.addCategoryListener(new CategoryListener() {
+
+			@Override
+			public void onSelect(Category category) {
+				System.out.println("Selected category: ");
+				System.out.println(category);
+				setCategoryImage(category.getImage());
+			}
+
+			@Override
+			public void onNoneSelected() {
+				// Set default image
+			}
+		});
     }
 }
